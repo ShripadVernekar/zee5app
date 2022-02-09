ab2bfd97460d87db6ea91ad0dc30f53f7625810a
Shripad_V <shripadvernekar2000@gmail.com>
2022-02-09 17:41:39 +0530

    09/02/22

--- zee5app-springboot/src/main/java/com/zee/zee5app/aop/UserServiceAspect.java
+++ zee5app-springboot/src/main/java/com/zee/zee5app/aop/UserServiceAspect.java
@@ -11,7 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
-@Aspect // container where u will hold ur app code
+@Aspect // container where u will hold your app code
 public class UserServiceAspect {
 
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
--- zee5app-springboot/src/main/java/com/zee/zee5app/controller/SubscriptionController.java
+++ zee5app-springboot/src/main/java/com/zee/zee5app/controller/SubscriptionController.java
@@ -23,49 +23,9 @@ import com.zee.zee5app.exception.IdNotFoundException;
 import com.zee.zee5app.service.UserService;
 
 @RestController
-//REST API: RESPONSE wherever we have to share the response that method  must be marked with @ResponseBody
 
-//1000emethods ---> @Responsebody ---> 1000times so they introduced @RestController
-@RequestMapping("/users")
+@RequestMapping("/ubscriptions")
 public class SubscriptionController {
 
-	// adding user info into the table
-	// info will be shared by the client in terms of JSON object
-	// do we need to read that json Object? ===> yes 15 // where is this json object
-	// is available in request? => requestbody
-	// do we need to read that requestbody content ? yes
-
-	// do we need to transform json object to Java object?yes ==> @RequestBody
-	@Autowired
-	UserService userService;
-
-	@PostMapping("/addUser")
-	public ResponseEntity<?> addUser(@Valid @RequestBody Register register) throws AlreadyExistsException {
-
-		Register result;
-
-		result = userService.addUser(register);
-		System.out.println(result);
-		return ResponseEntity.status(201).body(result);
-
-	}
-
-	@GetMapping("/{id}")
-	public ResponseEntity<?> getUserById(@PathVariable("id") String id) throws IdNotFoundException{
-		Register register = userService.getUserById(id);
-		return ResponseEntity.ok(register);
-	}
-
-	@GetMapping("/all")
-	public ResponseEntity<?> getAllUsers() {
-		Optional<List<Register>> optional = userService.getAllUserDetails();
-		
-		if(optional.isEmpty()) {
-			Map<String, String> hashMap = new HashMap<>();
-			hashMap.put("message", "No record found");
-			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
-		}
-		return ResponseEntity.ok(optional.get());
-	}
 	
 }
